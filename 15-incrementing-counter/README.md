cb30d6b6ca32147c4fc100c6ca39fe005389df88
HanTo1 <=>
2021-01-15 16:51:58 +0700

    todo

--- 15-incrementing-counter/README.md
+++ 15-incrementing-counter/README.md
@@ -3,4 +3,10 @@
 ## Date: 15 - 1 - 2021
 
 ### Da hoc duoc:
-- 
+- tao function
+  - lay data-target
+  - tao c de chuyen counter sang number
+  - tao bien buoc nhay
+  - c < buoc nhay: c + buoc nhay goi lai function
+  - else cho counter = target
+- goi function de chay
--- 15-incrementing-counter/script.js
+++ 15-incrementing-counter/script.js
@@ -3,21 +3,19 @@ const counters = document.querySelectorAll(".counter");
 counters.forEach((counter) => {
   counter.innerText = "0";
 
-  const updateCounter = () => {
+  const updateCount = () => {
     const target = +counter.getAttribute("data-target");
-    console.log('target: ', target);
     const c = +counter.innerText;
 
     const increment = target/200;
-
-    if(c < target) {
+    if(c < target){
       counter.innerText = `${Math.ceil(c + increment)}`;
-      setTimeout(updateCounter, 1);
+      setTimeout(updateCount, 1);
     }
-    else {
+    else{
       counter.innerText = target;
     }
-  }
+  };
 
-  updateCounter();
+  updateCount();
 });
